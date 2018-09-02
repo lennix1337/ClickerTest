d7a2093c041884d5f07663ba0221889db1cc4ec1
lennnix1337 <swlucas05@gmail.com>
2018-09-02 17:12:54 -0300

    background de floresta adicionado no game.js

--- src/game.js
+++ src/game.js
@@ -3,8 +3,22 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '');
 game.state.add('play', {
     preload: function() {
         game.load.image('skeleton', 'assets/allacrost_enemy_sprites/skeleton.png');
+        this.game.load.image('forest-back', 'assets/parallax_forest_pack/layers/parallax-forest-back-trees.png');
+        this.game.load.image('forest-lights', 'assets/parallax_forest_pack/layers/parallax-forest-lights.png');
+        this.game.load.image('forest-middle', 'assets/parallax_forest_pack/layers/parallax-forest-middle-trees.png');
+        this.game.load.image('forest-front', 'assets/parallax_forest_pack/layers/parallax-forest-front-trees.png');
     },
     create: function() {
+    var state = this;
+ 
+    this.background = this.game.add.group();
+    // setup each of our background layers to take the full screen
+    ['forest-back', 'forest-lights', 'forest-middle', 'forest-front']
+    .forEach(function(image) {
+        var bg = state.game.add.tileSprite(0, 0, state.game.world.width,
+            state.game.world.height, image, '', state.background);
+        bg.tileScale.setTo(4,4);
+    });
         var skeletonSprite = game.add.sprite(450, 290, 'skeleton');
         skeletonSprite.anchor.setTo(0.5, 0.5);
     },
